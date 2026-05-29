6ef73e864efd25c78d1274789e23c8f7eb67ad19
dmgnr <me@dgnr.us>
2026-05-30 03:06:36 +0700

    fix: migrate name to pname

--- flake.nix
+++ flake.nix
@@ -3,42 +3,47 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: {
-    packages.x86_64-linux = {
-      thorium = let
-        pkgs = import nixpkgs {system = "x86_64-linux";};
-        name = "thorium";
-        version = "M138.0.7204.303 - 65";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Alex313031/thorium/releases/download/M138.0.7204.303/Thorium_Browser_138.0.7204.303_AVX2.AppImage";
-          sha256 = "sha256-sXzUgqZ9loprBCObHXLRjkW15EzFFMBbqqqxuQ+ZIjA=";
-        };
-        appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
-      in
-        pkgs.appimageTools.wrapType2 {
-          inherit name version src;
-          extraInstallCommands = ''
-            install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
-            install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
-            substituteInPlace $out/share/applications/thorium-browser.desktop \
-            --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
-          '';
-        };
-      # AVX is compatible with most CPUs
-      default = self.packages.x86_64-linux.thorium;
-    };
-
-    apps.x86_64-linux = {
-      thorium = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.thorium}/bin/thorium";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    {
+      packages.x86_64-linux = {
+        thorium =
+          let
+            pkgs = import nixpkgs { system = "x86_64-linux"; };
+            pname = "thorium";
+            version = "M138.0.7204.303 - 65";
+            src = pkgs.fetchurl {
+              url = "https://github.com/Alex313031/thorium/releases/download/M138.0.7204.303/Thorium_Browser_138.0.7204.303_AVX2.AppImage";
+              sha256 = "sha256-sXzUgqZ9loprBCObHXLRjkW15EzFFMBbqqqxuQ+ZIjA=";
+            };
+            appimageContents = pkgs.appimageTools.extractType2 {
+              inherit pname src version;
+            };
+          in
+          pkgs.appimageTools.wrapType2 {
+            inherit pname version src;
+            extraInstallCommands = ''
+              install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
+              install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
+              substituteInPlace $out/share/applications/thorium-browser.desktop \
+              --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname} %U'
+            '';
+          };
+        # AVX is compatible with most CPUs
+        default = self.packages.x86_64-linux.thorium;
       };
 
-      default = self.apps.x86_64-linux.thorium;
+      apps.x86_64-linux = {
+        thorium = {
+          type = "app";
+          program = "${self.packages.x86_64-linux.thorium}/bin/thorium";
+        };
+
+        default = self.apps.x86_64-linux.thorium;
+      };
     };
-  };
 }
